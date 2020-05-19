1185c9ebf6d01869c3cb40d5aceeac9f954189ad
Mike Haggerty <ghoti143@gmail.com>
2020-05-19 11:58:20 -0400

    adding test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,27 +4,40 @@
 // utility that Flutter provides. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
+import 'package:covid_tracker/covidStates.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:covid_tracker/ListViewCovidStates.dart';
 
-import 'package:covid_tracker/main.dart';
+Widget makeTestableWidget({Widget child}) {
+    return MaterialApp(
+      home: Material(
+        child: child,
+      )
+    );
+}
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('State renders in list', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    final List<CovidState> testStates = [
+      new CovidState(abbreviation: 'VA', deaths: 100, recovered: 5, positive: 5000)
+    ];
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    final widget = makeTestableWidget(
+      child: ListViewCovidStates(covidStates: testStates),
+    );
 
+    await tester.pumpWidget(widget);
+
+    // Verify that list renders correctly.
+    expect(find.text('positive: 5000'), findsOneWidget);
+    expect(find.text('deaths: 100'), findsOneWidget);
+    expect(find.text('recovered: 5'), findsOneWidget);
+    
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    //await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
 }
